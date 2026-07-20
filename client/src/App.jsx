30b012095283dc9c3bdfbab172640d01c49d0ed1
muhanif-dev <muhanif.dev@gmail.com>
2026-07-20 07:16:58 +0500

    Implemented reusable form handling with controlled inputs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,14 @@ import { getAllNotes } from "./services/noteService";
 
 function App() {
   const [notes, setNotes] = useState([]);
+  const [formData, setFormData] = useState({
+    title:"",
+    subject:"",
+    semester:"",
+    description:"",
+  });
+
+
 
   const fetchNotes = async () => {
     try {
@@ -17,9 +25,68 @@ function App() {
     fetchNotes();
   }, []);
 
+  const handleChange = (e) => {
+    const { name, value} = e.target;
+    setFormData({
+        ...formData,
+        [name]: value
+    })
+  }
+
 return (
     <div>
         <h1>Student Notes</h1>
+
+
+             <form>
+
+            <input
+                type="text"
+                placeholder="Title"
+                name="title"
+                value={formData.title}
+                onChange={handleChange}
+            />
+
+            <br /><br />
+
+            <input
+                type="text"
+                placeholder="Subject"
+                name="subject"
+                value={formData.subject}
+                onChange={handleChange}
+            />
+
+            <br /><br />
+
+            <input
+                type="number"
+                placeholder="Semester"
+                name="semester"
+                value={formData.semester}
+                onChange={handleChange}
+            />
+
+            <br /><br />
+
+            <textarea
+                name="description"
+                value={formData.description}
+                placeholder="Description"
+                onChange={handleChange}
+            />
+
+            <br /><br />
+
+            <button type="submit">
+                Create Note
+            </button>
+
+        </form>
+
+
+
 
         {notes.map((note) => (
             <div key={note._id}>
