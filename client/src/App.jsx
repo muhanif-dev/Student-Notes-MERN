9e585a679db4b9a6ba65548f76fef07e3c5805e2
muhanif-dev <muhanif.dev@gmail.com>
2026-07-20 06:25:08 +0500

    Update UI by backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,33 +2,35 @@ import { useEffect, useState } from "react";
 import { getAllNotes } from "./services/noteService";
 
 function App() {
+  const [notes, setNotes] = useState([]);
 
-    const [notes, setNotes] = useState([]);
+  const fetchNotes = async () => {
+    try {
+      const response = await getAllNotes();
+      setNotes(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    const fetchNotes = async () => {
-        try {
+  useEffect(() => {
+    fetchNotes();
+  }, []);
 
-            const response = await getAllNotes();
+return (
+    <div>
+        <h1>Student Notes</h1>
 
-            console.log(response);
-
-            setNotes(response.data);
-
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    useEffect(() => {
-        fetchNotes();
-    }, []);
-
-    return (
-        <div>
-            <h1>Student Notes</h1>
-        </div>
-    );
-
+        {notes.map((note) => (
+            <div key={note._id}>
+                <h2>{note.title}</h2>
+                <p>Subject: {note.subject}</p>
+                <p>Semester: {note.semester}</p>
+                <hr />
+            </div>
+        ))}
+    </div>
+);
 }
 
 export default App;
